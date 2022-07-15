54f02e53fa51b465ddcefcbcbf2761f37a99e6db
Rajesh1951 <rajesh2oo1code@gmail.com>
2022-07-15 23:44:58 +0530

    cloudDB

--- src/db/conect.js
+++ src/db/conect.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/registration")
+mongoose.connect("mongodb+srv://rajesh:WtoNMGmNp7Yvd1Un@cluster0.8oqsh.mongodb.net/registration?retryWrites=true&w=majority")
     .then(() => console.log('connection sucessful'))
     .catch(() => console.log("connections failed"))
