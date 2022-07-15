2a4a6e03919aab6d32373aab573eba16ffb21a15
Rajesh1951 <rajesh2oo1code@gmail.com>
2022-07-16 00:11:50 +0530

    finifhing

--- src/app.js
+++ src/app.js
@@ -3,7 +3,14 @@ const hbs = require('hbs');
 const ejs = require('ejs');
 const path = require('path');
 
-require("./db/conect");
+// mongoose connection
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb+srv://rajesh:WtoNMGmNp7Yvd1Un@cluster0.8oqsh.mongodb.net/registration?retryWrites=true&w=majority")
+    .then(() => console.log('connection sucessful'))
+    .catch(() => console.log("connections failed"))
+
+// require("./db/conect");
 const Register = require('./models/regis')
 const port = process.env.PORT || 8000;
 
